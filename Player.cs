cfab57fe40beaced3e6abdea34d296233659d50c
dominic-taylor <dataylorx@gmail.com>
2016-07-23 17:28:10 +1200

    Add player and wall scripts

---
+++ Player.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject {
+
+	public float restartLevelDelay = 1f;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public int wallDamage = 1;
+
+
+	private Animator animator;
+	private int food;
+
+	// Use this for initialization
+	protected override void Start () {
+		animator = GetComponent<Animator> ();
+
+		food = GameManger.instance.playerFoodPoints;
+		base.Start ();
+	}
+
+	private void OnDisable(){
+	
+		GameManger.instance.playerFoodPoints = food;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!GameManger.instance.playersTurn)
+			return;
+
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int)(Input.GetAxisRaw ("Horizontal"));
+		vertical = (int)(Input.GetAxisRaw ("Vertical"));
+
+		if (horizontal != 0) {
+			vertical = 0;
+		}
+
+		if(horizontal != 0 || vertical != 0)
+		{
+			AttemptMove<Wall>(horizontal, vertical);
+		}
+	}
+
+	protected override void AttemptMove<T> (int xDir, int yDir)
+	{
+		food--;
+
+		base.AttemptMove<T> (xDir, yDir);
+
+		RaycastHit2D hit;
+
+		if (Move (xDir, yDir, out hit)) {
+			//ca;; sfx
+		
+		}
+		CheckIfGameOver ();
+
+		GameManger.instance.playersTurn = false;
+	}
+
+	protected override void OnCantMove<T>(T component)
+	{
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall (wallDamage);
+		animator.SetTrigger ("playerChop");
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Exit") {
+			Invoke ("Restart", restartLevelDelay);
+			enabled = false;
+		} else if (other.tag == "Food") {
+			food += pointsPerFood;
+			other.gameObject.SetActive (false);
+		}else if (other.tag == "Soda") {
+			food += pointsPerSoda;
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	private void Restart()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void LoseFood(int loss)
+	{
+		animator.SetTrigger ("playerHit");
+		food -= loss;
+		CheckIfGameOver ();
+	}
+
+	private void CheckIfGameOver()
+	{
+		if (food <= 0) {
+			GameManger.instance.GameOver ();
+		}
+	}
+}
